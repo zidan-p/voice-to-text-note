84918fba9a757f8b47b3280c3b2a2f800dffa4d1
zidan putra rahman <zidanputrarahman153@gmail.com>
2024-10-14 22:39:55 +0700

    more collapse functionality

--- src/component/collapse-list.tsx
+++ src/component/collapse-list.tsx
@@ -2,6 +2,7 @@ import { cn } from "../lib/cn";
 import TrashIcon from "./../assets/trash.svg?react";
 import PencilIcon from "../assets/pencil.svg?react";
 import ChevronLeft from "../assets/chevron-left.svg?react"
+import { useRef, useState } from "react";
 
 interface CollapseListProps {
   className?: string;
@@ -9,6 +10,9 @@ interface CollapseListProps {
 
 
 export function CollapseList(props: CollapseListProps){
+
+  const [isCollapse, setIsCollapse] = useState(true);
+  const contentRef = useRef<HTMLParagraphElement | null>(null);
 
   return(
     <div className={cn(props.className, "hover:bg-slate-200 p-2 rounded group")}>
@@ -28,12 +32,20 @@ export function CollapseList(props: CollapseListProps){
           <button className="p-2 bg-red-300 rounded text-red-800 active:text-red-700 active:bg-red-200">
             <TrashIcon className="w-5" />
           </button>
-          <button className="p-2 rounded active:bg-slate-100">
-            <ChevronLeft className="w-5" />
+          <button className="p-2 rounded active:bg-slate-100" onClick={() => setIsCollapse(!isCollapse)}>
+            <ChevronLeft className={cn("w-5 transition", !isCollapse ? "-rotate-90" : "")} />
           </button>
         </div>
       </div>
-      <p className="hidden">
+      <p
+        ref={contentRef}
+        style={
+          isCollapse
+          ? { maxHeight: 0 }
+          : { maxHeight: contentRef.current?.scrollHeight }
+        }
+        className={cn("overflow-hidden transition-all")}
+      >
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate voluptatum, delectus sit sed ea explicabo deserunt, nulla mollitia alias molestiae illum? Molestiae in possimus debitis quidem assumenda, incidunt magni itaque!
       </p>
     </div>

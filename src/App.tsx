2e979e39f091ff197c6786de16ca5599b9e0a6e7
zidan putra rahman <zidanputrarahman153@gmail.com>
2024-10-14 22:00:34 +0700

    add svgr and build more structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container mx-auto min-h-screen bg-slate-100 py-4">
+        <div className="max-w-lg mx-auto p-1 bg-white rounded">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente, iste impedit expedita, magnam cupiditate assumenda magni sequi praesentium sunt aspernatur veniam aliquam distinctio, non neque temporibus tempora dolores esse. Voluptates.
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
